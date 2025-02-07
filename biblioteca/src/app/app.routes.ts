eff2758acc068113d57ebda177cd6752fa08133f
3st3fayaOM <eortega8634@ceuandalucia.es>
2025-02-07 18:25:31 +0100

    0702

--- biblioteca/src/app/app.routes.ts
+++ biblioteca/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LibrosComponent } from './libros/libros.component';
+import { LibroDetalleComponent } from './libro-detalle/libro-detalle.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'libros', component: LibrosComponent },
+  { path: 'libro-detalle', component: LibroDetalleComponent },
+  { path: '**', redirectTo: 'home' },
+];
