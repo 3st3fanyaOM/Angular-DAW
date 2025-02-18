b454fbb128bd7f1c8243fd4f54180e74c0cd343b
3st3fayaOM <eortega8634@ceuandalucia.es>
2025-02-18 20:56:25 +0100

    1802

---
+++ ejrepaso1/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { PostComponent } from './post/post.component';
+
+export const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'usuario', component: UsuarioComponent },
+  { path: 'post', component: PostComponent },
+];
---
+++ ejrepaso1/src/app/usuario/usuario.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-usuario',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './usuario.component.html',
+  styleUrl: './usuario.component.css'
+})
+export class UsuarioComponent {
+
+  buscarUsuario(){
+    
+  }
+}
